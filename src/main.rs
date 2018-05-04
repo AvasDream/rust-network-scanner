59a6a2d008060c85851dc2595d3338aa9e6cd29f
Vincent Grimmeisen <vincegrixc@gmail.com>
2018-05-04 13:35:53 +0200

    Cleaned code, just essentials

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,53 @@
+extern crate threadpool;
+
 use std::net::*;
 use std::env;
-use std::process;
-use std::sync::mpsc;
-use std::thread;
-/*
-- input validation
-*/
+use std::process; //exit(0)
+use std::sync::mpsc; //channel
+use threadpool::ThreadPool; //Threadpool, extern crate
 
 fn main() {
     let remote_target = "141.37.29.215";
-    let args: Vec<String> = parse_arguments();
-    println!("{},{},{}",args[0],args[1],args[2]);
-    let open_ports = scan(&args[0],&args[1],&args[2]);
-    println!("{}",open_ports[0]);
+    let open_ports = scan(remote_target,"0","100");
     for port in open_ports {
         println!("{} is open",port)
     }
 }
 
+
+pub fn scan(ip: &str, port_beginn: &str, port_end:&str)-> Vec<u32> {
+    let mut open_ports: Vec<u32> = vec![];
+    let port_beginn = port_beginn.parse::<u32>().unwrap();
+    let port_end = port_end.parse::<u32>().unwrap();
+    for port in port_beginn..port_end {
+        println!("Scanning Port {} on {}",port,ip);
+        if port_open(ip, &port.to_string()) {
+            open_ports.push(port);
+        }
+    }
+    open_ports
+}
+
+pub fn port_open(ip: &str, port: &str) -> bool {
+    let mut addr = ip.to_string();
+    addr.push_str(":");
+    addr.push_str(port);
+    if let Ok(stream) = TcpStream::connect(addr) {
+        true
+    } else {
+        false
+    }
+}
+
+#[allow(dead_code)]
+fn help() {
+    println!("usage:
+              main.rs <IP> <Port-Range>
+              Example:
+              main.rs 127.0.0.1 1-100");
+}
+
+#[allow(dead_code)]
 fn parse_arguments()-> Vec<String> {
     let args: Vec<String> = env::args().collect();
     let mut ret: Vec<String> = vec![];
@@ -44,86 +74,4 @@ fn parse_arguments()-> Vec<String> {
         }
     }
     ret
-}
-
-fn help() {
-    println!("usage:
-              main.rs <IP> <Port-Range>
-              Example:
-              main.rs 127.0.0.1 1-100");
-}
-
-pub fn scan(ip: &str, port_beginn: &str, port_end:&str)-> Vec<u32> {
-    let mut open_ports: Vec<u32> = vec![];
-    let port_beginn = port_beginn.parse::<u32>().unwrap();
-    let port_end = port_end.parse::<u32>().unwrap();
-
-    let (sender, receiver) = mpsc::channel();
-
-
-    for port in port_beginn..port_end {
-        if scan_port(ip, port) {
-            open_ports.push(port);
-        }
-    }
-
-
-
-/*
-
-    thread::spawn(move || {
-            match sender.send(port_open(ip,&port.to_string())) {
-                Ok(True) => {}, // everything good
-                Err(_) => {}, // we have been released, don't panic
-            }
-        });
-        match receiver.try_recv() {
-            Ok(True) => open_ports.push(port), // we have a connection
-            Err(_) => {
-                drop(receiver);
-            }
-        }
-    */
-
-
-    open_ports
-}
-pub fn port_open(ip: &str, port: &str) -> bool {
-    let mut addr = ip.to_string();
-    addr.push_str(":");
-    addr.push_str(port);
-    if let Ok(stream) = TcpStream::connect(addr) {
-        true
-    } else {
-        false
-    }
-}
-
-fn scan_port(host: &str, port: u32) -> bool {
-    let host = host.to_string();
-    let port = port;
-    let (sender: Result<Some()>, receiver: Result<Some()>) = mpsc::channel();
-    let t = thread::spawn(move || {
-        let mut addr = host.to_string();
-        addr.push_str(":");
-        addr.push_str(&port.to_string());
-        match sender.send(TcpStream::connect(addr)) {
-            Ok(()) => {}, // everything good
-            Err(_) => {}, // we have been released, don't panic
-        }
-    });
-
-    thread::sleep(std::time::Duration::new(5, 0));
-
-    match receiver.try_recv() {
-        Ok(Ok(handle)) => true, // we have a connection
-        Ok(Err(_)) => false, // connecting failed
-        Err(mpsc::TryRecvError::Empty) => {
-            drop(receiver);
-            drop(t);
-            // connecting took more than 5 seconds
-            false
-        },
-        Err(mpsc::TryRecvError::Disconnected) => unreachable!(),
-    }
 }
